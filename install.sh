9ff49ab253712eb06fea9b2cce468bd82a2dc851
Max Vasiliev <max@vasiliev.me>
2015-08-19 15:13:53 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Moving old ~/.vimrc and ~/.gvimrc to ~/._vim direcotory in case you ever need them."
-vimdir = ~/.vim
+vimdir = ~/.nvim
 # TODO refactor it to make a little bit more maintainable
 mkdir -p ~/._vim
 mv ~/.vimrc ~/._vim/.vimrc
@@ -7,16 +7,16 @@ mv ~/.gvimrc ~/._vim/.gvimrc
 
 
 echo "Linking vimrc and gvimrc."
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/nvimrc ~/.nvimrc
+ln -s ~/.vim/gnvimrc ~/.gnvimrc
 
-echo "Cloning bundles."
-mkdir -p ~/.vim/bundle
+# echo "Cloning bundles."
+# mkdir -p ~/.vim/bundle
 
 # clone vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # run bundle install
-vim +BundleInstall +qall
+# vim +BundleInstall +qall
 
-echo "All done!"
+# echo "All done!"
